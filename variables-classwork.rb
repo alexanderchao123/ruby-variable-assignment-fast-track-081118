fdb9ba1a9c2de99ebb3128b42dcfb9e7700b7f63
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-11 17:46:33 +0000

    automatically backed up by learn

---
+++ variables-classwork.rb
@@ -0,0 +1,21 @@
+# this is a variable that's a string
+dog = "Wolf"
+# a number (Integer)
+my_age = 30
+# an array
+some_stuff_array = [1, 2, "fish"]
+
+
+puts dog
+puts my_age
+puts some_stuff_array
+
+# update data
+#  a year has gone by
+my_age = my_age + 1
+# same thing
+my_age += 1
+
+# interpolation
+puts "Now, my age is " + "#{my_age}."
+# puts my_age
