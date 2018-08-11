e74423990e7d60392c8ae4e1cf959529e34e15c0
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-11 17:53:14 +0000

    automatically backed up by learn

--- default-arguments.rb
+++ default-arguments.rb
@@ -13,3 +13,11 @@
 # Create a method compute_months that takes in these three variables (name, hometown, and age)
 # #comput_months should puts a statement like this:
 # "I'm Howard, I'm from Columbus, and I've been alive for ___ months!"
+
+name = "Alexander"
+hometown = "Great Neck"
+age = 28
+
+def compute_months(name, hometown, age)
+  puts "I'm #{name}, I'm from #{hometown}, and I've been alive for #{28 * 12} months!"
+end
