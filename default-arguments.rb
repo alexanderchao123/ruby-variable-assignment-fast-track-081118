6c1cb5b744d68341b2ffb1511871c6bce4a1618e
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-11 17:51:13 +0000

    automatically backed up by learn

--- default-arguments.rb
+++ default-arguments.rb
@@ -1,5 +1,15 @@
-def greeting (greeting = "Hola!")
-  puts greeting
-end
+# def greeting (greeting = "Hola!")
+#   puts greeting
+# end
+#
+# greeting "bienvenidos!"
 
-greeting "bienvenidos!"
+# Create a variable called name and give it a value
+
+# Create a variable called hometown and give it a value
+
+# Create a variable called age and give it a value
+
+# Create a method that takes in these three variables (name, hometown, and age)
+# #comput_months should puts a statement like this:
+# "I'm Howard, I'm from Columbus, and I've been alive for ___ months!"
